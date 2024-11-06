2a2b683028e1cea65b30872d6845acc579419d31
Natalyapan <pannatalya1111@gmail.com>
2024-11-06 21:39:51 +0100

    person2commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,8 +1,8 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("This is the project version 4");
+        System.out.println("This is the project version 6 made by person 2");
 
     }
 }
-//done with version four
+//done with version six
