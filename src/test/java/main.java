73615ff751982a7b0e0680a331ed0797f2834d04
Natalyapan <pannatalya1111@gmail.com>
2024-11-06 15:06:13 +0100

    fouth commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("This is the project version 3");
+        System.out.println("This is the project version 1");
 
     }
 }
